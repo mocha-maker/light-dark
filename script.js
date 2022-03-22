488142e047e419aa8e1b817af3702d2603a78167
fpyc <fpyc@wgu.edu>
2022-03-22 16:05:16 -0400

    Added Theme Constants

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Toggle Elements
+const DARK_THEME = 'dark'
+const LIGHT_THEME = 'light'
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const theme = document.documentElement;
 
@@ -17,10 +19,12 @@ function changeImage(color) {
     image3.src = `./images/undraw_new_ideas_${color}.svg`;
 }
 
-function toggleTheme(isDark) {
+function toggleTheme(isTheme) {
+    let isDark = (isTheme == DARK_THEME) ? true : false;
     // Change Navigation and Textbox
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    textBox.style.color = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
 
     // Change Toggle
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
@@ -28,7 +32,7 @@ function toggleTheme(isDark) {
             toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
 
     // Change images
-    isDark ? changeImage('dark') : changeImage('light');
+    isDark ? changeImage(DARK_THEME) : changeImage(DARK_THEME);
 }
 
 // Switch Theme
@@ -36,14 +40,14 @@ function switchTheme(event) {
 
     if (event.target.checked) {
         // Set root theme
-        theme.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme','dark')
-        toggleTheme(true);
+        theme.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME)
+        toggleTheme(DARK_THEME);
     } else {
         // Set root theme
-        theme.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme','light')    
-        toggleTheme(false);
+        theme.setAttribute('data-theme', LIGHT_THEME );
+        localStorage.setItem('theme', LIGHT_THEME);
+        toggleTheme(LIGHT_THEME);
     }
 }
 
@@ -55,8 +59,8 @@ const currentTheme = localStorage.getItem('theme');
 
 if(currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
+    if (currentTheme === DARK_THEME) {
         toggleSwitch.checked = true;
-        toggleTheme(true);
+        toggleTheme(DARK_THEME);
     }
 }
